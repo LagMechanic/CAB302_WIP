fdbe30e6df49ff19e7fa121067d1a240190bb632
Luis <luisctorres0305@gmail.com>
2024-04-26 21:03:46 +1000

    added insertProfile(), updateProfile(), deleteProfile()

--- src/main/java/com/zenbrowser/a1/model/FocusProfile/ProfileDAO.java
+++ src/main/java/com/zenbrowser/a1/model/FocusProfile/ProfileDAO.java
@@ -12,14 +12,39 @@ public class ProfileDAO {
     }
 
     public void insertProfile(Profile profile) {
-        String sql = "INSERT INTO profiles (ProfileName, Website_id) VALUES (?, ?)";
+        String sql = "INSERT INTO profiles (profileName, websiteId, profileId) VALUES (?, ? ,?)";
 
         try (PreparedStatement statement = connection.prepareStatement(sql)){
             statement.setString(1, profile.getProfileName());
             statement.setInt(2, profile.getWebsite().getId());
+            statement.setInt(3, profile.getId());
         }
         catch (SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    public void updateProfile(Profile profile) {
+        String sql = "UPDATE profiles SET profileName=?, websiteId=? WHERE id=?";
+
+        try(PreparedStatement statement = connection.prepareStatement(sql)){
+            statement.setString(1, profile.getProfileName());
+            statement.setInt(2, profile.getWebsite().getId());
+            statement.setInt(3, profile.getId());
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public void deleteProfile(int id) throws SQLException {
+        String sql = "DELETE FROM profiles WHERE id=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        }
+    }
+
+    public Profile getProfileById(int id){
+        String sql = "SELECT * FROM profiles WHERE id=?";
     }
 }
