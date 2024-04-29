8e2284e5fcf35755895b2013c4222745a9f59cc1
JoshK28 <joshkeenan28@gmail.com>
2024-04-29 12:13:11 +1000

    tested HistoryRecordDAO update method

--- src/main/java/com/zenbrowser/a1/model/FocusProfile/IProfileDAO.java
+++ src/main/java/com/zenbrowser/a1/model/FocusProfile/IProfileDAO.java
@@ -1,11 +1,10 @@
 package com.zenbrowser.a1.model.FocusProfile;
 
 public interface IProfileDAO {
-<<<<<<< HEAD
+
     Profile insertProfile(Profile profile);
     void updateProfile(Profile profile);
     void deleteProfile(int id);
     void getProfile(int id);
-=======
->>>>>>> origin/master
+
 }
