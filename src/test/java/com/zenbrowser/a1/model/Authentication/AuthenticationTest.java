1bbcda03951d05863b48c0738d7a6d3f14e37c81
dom-cutajar <domcutajar@outlook.com>
2024-04-29 17:21:13 +1000

    Moved AuthenticationTest to tests folder, passes all tests (although other module tests don't compile)

---
+++ src/test/java/com/zenbrowser/a1/model/Authentication/AuthenticationTest.java
@@ -0,0 +1,77 @@
+package com.zenbrowser.a1.model.Authentication;
+
+import com.zenbrowser.a1.model.User.IUserDAO;
+
+import com.zenbrowser.a1.model.User.User;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AuthenticationTest {
+    private Authentication auth;
+
+    /**
+     * Create a new user (username1, password1)
+     */
+    @BeforeEach
+    public void setup() {
+        try {
+            IUserDAO usersDAO = new MockUsersDAO();
+            auth = Authentication.getTestInstance();
+            auth.signup(new User("username1", "password1"));
+        } catch (UserAlreadyExists e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void validNewUser() {
+
+        try {
+            auth.signup(new User("username2", "password2"));
+            auth.login(new User("username2", "password2"));
+            assertEquals(true, auth.userLoggedIn());
+        } catch (InvalidCredentials | UserAlreadyExists e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void validUsernamePassword() {
+        try {
+            auth.login(new User("username1", "password1"));
+            assertEquals(true, auth.userLoggedIn());
+        } catch (InvalidCredentials e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void invalidUsernamePassword() {
+        assertThrows(
+                InvalidCredentials.class,
+                () -> auth.login(new User("not a username", "not a password"))
+        );
+    }
+
+    @Test
+    public void userAlreadyExists() {
+        assertThrows(
+                UserAlreadyExists.class,
+                () -> auth.signup(new User("username1", "password1"))
+        );
+    }
+
+
+    @Test
+    public void logout() {
+        try {
+            auth.login(new User("username1", "password1"));
+            assertEquals(true, auth.userLoggedIn());
+            auth.logout();
+            assertEquals(false, auth.userLoggedIn());
+        } catch (InvalidCredentials e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
