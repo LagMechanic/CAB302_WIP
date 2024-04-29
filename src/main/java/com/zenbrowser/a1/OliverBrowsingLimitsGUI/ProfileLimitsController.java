62567e9441d27fce9caa222ba1c60f0d7dc4dba5
scxlly05 <oliverscully05@gmail.com>
2024-04-29 14:03:19 +1000

    tested HistoryRecordDAO update method

---
+++ src/main/java/com/zenbrowser/a1/OliverBrowsingLimitsGUI/ProfileLimitsController.java
@@ -0,0 +1,33 @@
+package com.zenbrowser.a1.OliverBrowsingLimitsGUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ProfileLimitsController {
+
+    @FXML
+    private TextField urlField;
+
+    @FXML
+    private TextField limitField;
+
+    @FXML
+    private TableView<com.zenbrowser.a1.OliverBrowsingLimitsGUI.UrlLimit> urlTable;
+
+    public void addUrlAndLimit() {
+        String url = urlField.getText();
+        String limit = limitField.getText();
+        if (!url.isEmpty() && !limit.isEmpty()) {
+            urlTable.getItems().add(new com.zenbrowser.a1.OliverBrowsingLimitsGUI.UrlLimit(url, limit));
+            urlField.clear();
+            limitField.clear();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("URL or Limit cannot be empty");
+            alert.showAndWait();
+        }
+    }
+
+}
---
+++ src/main/java/com/zenbrowser/a1/OliverBrowsingLimitsGUI/UrlLimit.java
@@ -0,0 +1,4 @@
+package com.zenbrowser.a1.OliverBrowsingLimitsGUI;
+
+public record UrlLimit(String url, String limit) {
+}
