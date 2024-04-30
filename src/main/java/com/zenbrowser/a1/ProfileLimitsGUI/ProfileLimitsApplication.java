f94fbbf16dd31f41e73a52caca65dddd7e7e008e
scxlly05 <oliverscully05@gmail.com>
2024-04-30 10:08:06 +1000

    Testing ProfileLimits fxml

--- src/main/java/com/zenbrowser/a1/ProfileLimitsGUI/ProfileLimitsApplication.java
+++ src/main/java/com/zenbrowser/a1/ProfileLimitsGUI/ProfileLimitsApplication.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 public class ProfileLimitsApplication extends Application {
 
+
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(ProfileLimitsApplication.class.getResource("ProfileLimits.fxml"));
@@ -15,6 +17,7 @@ public class ProfileLimitsApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
 
 
     public static void main(String[] args) {
