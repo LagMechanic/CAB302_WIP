42ba2082f810a4c02ea423ba2594e6dc00cccdc7
JoshK28 <joshkeenan28@gmail.com>
2024-05-01 08:46:30 +1000

    Intialise connection for abstract class

--- src/main/java/com/zenbrowser/a1/Controller/ControllerAbstract.java
+++ src/main/java/com/zenbrowser/a1/Controller/ControllerAbstract.java
@@ -1,10 +1,14 @@
 package com.zenbrowser.a1.Controller;
 
 import com.zenbrowser.a1.model.Authentication.IAuthentication;
+import com.zenbrowser.a1.model.BrowserUsage.HistoryRecordDAO;
 import com.zenbrowser.a1.model.BrowserUsage.IHistoryRecordDAO;
 import com.zenbrowser.a1.model.FocusProfile.IProfileDAO;
+import com.zenbrowser.a1.model.FocusProfile.ProfileDAO;
 import com.zenbrowser.a1.model.User.IUserDAO;
+import com.zenbrowser.a1.model.User.SqliteUserDAO;
 import com.zenbrowser.a1.model.Website.ISiteDAO;
+import com.zenbrowser.a1.model.Website.SiteDAO;
 
 public abstract class ControllerAbstract {
     protected IUserDAO UserDAO;
@@ -14,4 +18,12 @@ public abstract class ControllerAbstract {
     protected IProfileDAO ProfileDAO;
 
     protected IHistoryRecordDAO HistoryDAO;
+
+    ControllerAbstract()
+    {
+        UserDAO = new SqliteUserDAO();
+        SiteDAO = new SiteDAO();
+        ProfileDAO = new ProfileDAO();
+        HistoryDAO = new HistoryRecordDAO();
+    }
 }
