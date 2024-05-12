8f5c3fbbaba4ae6fb478263bfc188cffd2c21880
JoshK28 <joshkeenan28@gmail.com>
2024-05-12 11:40:55 +1000

    Functional history page shows history of all pages.

--- src/main/java/com/zenbrowser/a1/Controller/HistoryController.java
+++ src/main/java/com/zenbrowser/a1/Controller/HistoryController.java
@@ -5,16 +5,20 @@ import javafx.event.EventHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 import com.zenbrowser.a1.model.browserTab;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebHistory;
 
-public class HistoryController extends ParentController{
+public class HistoryController extends ParentController {
+    @FXML
+    private VBox historyContainer;
+
     public void initialize() {
         for (int i = 0; i < browserTab.getAllEntries().size(); i++) {
             WebHistory.Entry entry = browserTab.getAllEntries().get(i);
-            new Label().setText(entry.toString());
-
+            Label entryLabel = new Label(entry.toString());
+            historyContainer.getChildren().add(entryLabel);
         }
     }
-
 }
