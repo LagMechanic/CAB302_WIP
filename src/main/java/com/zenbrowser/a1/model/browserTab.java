e2504c6efb310cb246b3b13e1524fba4c9100b56
JoshK28 <joshkeenan28@gmail.com>
2024-05-12 11:19:27 +1000

    Within browserTab class, added fullhistory list which collates all Webhistory instances.

--- src/main/java/com/zenbrowser/a1/model/browserTab.java
+++ src/main/java/com/zenbrowser/a1/model/browserTab.java
@@ -5,14 +5,24 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class browserTab extends Tab {
     private WebView webView;
     private WebEngine webEngine;
+    private WebHistory webHistory;
 
-    public browserTab(String tabname){
+    private static List<WebHistory> fullHistory = new ArrayList<>();
+
+    public browserTab(String tabname) {
+
         super(tabname);
         webView = new WebView();
         webEngine = webView.getEngine();
+        webHistory = webEngine.getHistory();
+        fullHistory.add(webHistory);
+
     }
 
     public WebEngine getWebEngine() {
@@ -24,6 +34,14 @@ public class browserTab extends Tab {
     }
 
     public WebHistory getHistory() {
-        return webEngine.getHistory();
+        return webHistory;
+    }
+
+    public List<WebHistory.Entry> getAllEntries() {
+        List<WebHistory.Entry> allEntries = new ArrayList<>();
+        for (WebHistory history : fullHistory) {
+            allEntries.addAll(history.getEntries());
+        }
+        return allEntries;
     }
 }
