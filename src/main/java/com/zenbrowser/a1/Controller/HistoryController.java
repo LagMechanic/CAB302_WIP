9aa42c340c2df8f161a541c19240f281384c702e
JoshK28 <joshkeenan28@gmail.com>
2024-05-13 17:18:33 +1000

    Refactor history records into method.

--- src/main/java/com/zenbrowser/a1/Controller/HistoryController.java
+++ src/main/java/com/zenbrowser/a1/Controller/HistoryController.java
@@ -2,6 +2,7 @@ package com.zenbrowser.a1.Controller;
 import com.zenbrowser.a1.model.BrowserUsage.HistoryRecord;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import java.util.List;
@@ -9,20 +10,39 @@ import java.util.List;
 
 public class HistoryController extends ParentController {
     @FXML
+    private GridPane historyGrid;
+
+    @FXML
     private VBox historyContainer;
 
     private List<HistoryRecord> records;
+
+
     public void initialize() {
         records = HistoryDAO.getAllUserHistoryRecords(super.getCurrentUser());
-        for (int i = records.size() - 1; i >= 0 ; i--) {
-            Label Title = new Label(records.get(i).getSite());
-            Label URL = new Label(records.get(i).getURL());
-            Label Time = new Label(records.get(i).getHistoryRecordDateTime().toString());
 
-            HBox entryContainer = new HBox();
-            entryContainer.setSpacing(10);
-            entryContainer.getChildren().addAll(Title, URL, Time);
-            historyContainer.getChildren().add(entryContainer);
+        PopulateRecords();
+    }
+
+
+    private void PopulateRecords(){
+        int row = 1;
+
+        for (int i = records.size() - 1; i >= 0; i--) {
+            HistoryRecord record = records.get(i);
+
+            Label siteLabel = new Label(record.getSite());
+            Label URLLabel = new Label(record.getURL());
+            Label timeLabel = new Label(record.getHistoryRecordDateTime().toString());
+
+            // Create a new GridPane for each record
+            GridPane recordGrid = new GridPane();
+            recordGrid.add(siteLabel, 0, 0);
+            recordGrid.add(URLLabel, 0, 1);
+            recordGrid.add(timeLabel, 0, 2);
+
+            // Add the recordGrid to the historyContainer
+            historyContainer.getChildren().add(recordGrid);
         }
     }
 }
