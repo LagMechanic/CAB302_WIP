96758c83340d53bed1a0698c2bfc96f16c4a2b42
JoshK28 <joshkeenan28@gmail.com>
2024-05-23 01:22:26 +1000

    Refactored code, and abstracted to methods to improve readability.

--- src/main/java/com/zenbrowser/a1/Controller/MainControllers/ProfileLimitsController.java
+++ src/main/java/com/zenbrowser/a1/Controller/MainControllers/ProfileLimitsController.java
@@ -26,7 +26,6 @@ public class ProfileLimitsController extends ParentController {
     private TextField urlField;
     @FXML
     private Button UrlLimitData;
-
     @FXML
     private ComboBox<String> profileBox;
 
@@ -41,68 +40,23 @@ public class ProfileLimitsController extends ParentController {
     @FXML
     private TableColumn <Profile,Void> deleteColumn;
 
-
     private final ObservableList<Profile> profileData = FXCollections.observableArrayList();
+
+
+
+    //Methods
+
 
     public void initialize() {
 
-        hoursBox.setItems(FXCollections.observableArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18,19,20,21,22,23,24));
-        minutesBox.setItems(FXCollections.observableArrayList(0,5,10,15,20,25,30,35,40,45,50,55));
+        PopulateComboBox();
 
-        profileColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getProfileName()));
-        urlColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getSiteURL()));
-        limitColumn.setCellValueFactory(cell -> new SimpleObjectProperty<>(cell.getValue().getBlockedUntil()));
+        LinkTable();
 
-        deleteColumn.setCellFactory(cell -> new TableCell<>() {
-            private final Button deleteButton;
-            {
-                deleteButton = new Button("Delete");
-                deleteButton.setOnAction(evt -> {
-                    Profile entry = getTableRow().getItem();
-
-                    try {
-                        ProfileDAO.deleteProfile(entry.getId());
-                        loadProfilesTable();
-
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-
-            @Override
-            protected void updateItem(Void item, boolean empty) {
-                super.updateItem(item, empty);
-                setGraphic(empty ? null : deleteButton);
-            }
-        });
-
-        tbData.setItems(profileData);
         loadProfilesTable();
     }
 
-    private void loadProfilesTable()  {
 
-        String selectedprofile = profileBox.getSelectionModel().getSelectedItem();
-        List<Profile> profilesEntries;
-
-        if (selectedprofile == null){
-            profilesEntries = ProfileDAO.getUserProfiles(getCurrentUser());
-        }else{
-            profilesEntries = ProfileDAO.getSingleProfile(getCurrentUser(), selectedprofile);
-        }
-
-        profileData.clear();
-        for (Profile profile : profilesEntries) {
-            profileData.add(profile);
-
-            String[] blockedDuration = profile.getBlockedDuration();
-            // Limit has expired
-            if (blockedDuration == null) {
-
-            }
-        }
-    }
 
     @FXML
     private void addUrlAndLimit() {
@@ -136,4 +90,66 @@ public class ProfileLimitsController extends ParentController {
 
     @FXML
     private void changeProfile() {  loadProfilesTable();}
+
+
+    private void loadProfilesTable()  {
+
+        String selectedprofile = profileBox.getSelectionModel().getSelectedItem();
+        List<Profile> profilesEntries;
+
+        if (selectedprofile == null){
+            profilesEntries = ProfileDAO.getUserProfiles(getCurrentUser());
+        }else{
+            profilesEntries = ProfileDAO.getSingleProfile(getCurrentUser(), selectedprofile);
+        }
+
+        profileData.clear();
+        for (Profile profile : profilesEntries) {
+            profileData.add(profile);
+
+            String[] blockedDuration = profile.getBlockedDuration();
+            // Limit has expired
+            if (blockedDuration == null) {
+
+            }
+        }
+    }
+
+    private void LinkTable() {
+        profileColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getProfileName()));
+        urlColumn.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getSiteURL()));
+        limitColumn.setCellValueFactory(cell -> new SimpleObjectProperty<>(cell.getValue().getBlockedUntil()));
+
+        deleteColumn.setCellFactory(cell -> new TableCell<>() {
+            private final Button deleteButton;
+            {
+                deleteButton = new Button("Delete");
+                deleteButton.setOnAction(evt -> {
+                    Profile entry = getTableRow().getItem();
+
+                    try {
+                        ProfileDAO.deleteProfile(entry.getId());
+                        loadProfilesTable();
+
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                setGraphic(empty ? null : deleteButton);
+            }
+        });
+
+        tbData.setItems(profileData);
+    }
+
+    private void PopulateComboBox(){
+        hoursBox.setItems(FXCollections.observableArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18,19,20,21,22,23,24));
+        minutesBox.setItems(FXCollections.observableArrayList(0,5,10,15,20,25,30,35,40,45,50,55));
+    }
+
 }
