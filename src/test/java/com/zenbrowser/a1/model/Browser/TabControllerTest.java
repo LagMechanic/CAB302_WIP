2b8b8993702399cb5b448fecbd6d5ef1b5373454
Dom-Cutajar <n10928987@qut.edu.au>
2024-05-29 12:28:55 +1000

    Removed tests for Site (no longer used) and TabController (no useful tests)

--- src/test/java/com/zenbrowser/a1/model/Browser/TabControllerTest.java
+++ src/test/java/com/zenbrowser/a1/model/Browser/TabControllerTest.java
@@ -1,32 +1,32 @@
-package com.zenbrowser.a1.model.Browser;
-
-import com.zenbrowser.a1.Controller.MainControllers.TabController;
-
-import org.junit.Before;
-import org.junit.Test;
-
-
-public class TabControllerTest {
-
-    private TabController tabController;
-
-    @Before
-    public void setUp() {
-        tabController = new TabController();
-
-
-        // Mock any other dependencies as needed
-    }
-
-    @Test
-    public void testLoadPage() {
-        try {
-            String validUrl = "https://www.qut.edu.au/";
-            tabController.loadPage(validUrl);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
-
-// Add more tests for other methods in BrowserMain as needed
+//package com.zenbrowser.a1.model.Browser;
+//
+//import com.zenbrowser.a1.Controller.MainControllers.TabController;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//
+//public class TabControllerTest {
+//
+//    private TabController tabController;
+//
+//    @Before
+//    public void setUp() {
+//        tabController = new TabController();
+//
+//
+//        // Mock any other dependencies as needed
+//    }
+//
+//    @Test
+//    public void testLoadPage() {
+//        try {
+//            String validUrl = "https://www.qut.edu.au/";
+//            tabController.loadPage(validUrl);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
+//
+//// Add more tests for other methods in BrowserMain as needed
